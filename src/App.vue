bc8dc3f26f46deeb56ff0279c0de6d869ffe966b
Sathishkumar Thangavel <sathishkumar.thangavel@chargebee.com>
2021-12-09 12:07:20 +0530

    feat : Quiz app completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,129 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ctr">
+    <transition name="fade" mode="out-in">
+      <question
+        v-if="questionsAnswered < questions.length"
+        :questions="questions"
+        :questionsAnswered="questionsAnswered"
+        @question-answered="questionAnswered"
+      />
+      <result v-else :results="results" :totalCorrect="totalCorrect" />
+    </transition>
+    <button
+      v-if="questionsAnswered === questions.length"
+      type="button"
+      class="reset-btn"
+      @click="resetQuiz"
+    >
+      Reset
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Question from "./components/Questions.vue";
+import Result from "./components/Results.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Question,
+    Result,
+  },
+  data: function () {
+    return {
+      questionsAnswered: 0,
+      totalCorrect: 0,
+      questions: [
+        {
+          q: "What is 2 + 2?",
+          answers: [
+            {
+              text: "4",
+              is_correct: true,
+            },
+            {
+              text: "3",
+              is_correct: false,
+            },
+            {
+              text: "Fish",
+              is_correct: false,
+            },
+            {
+              text: "5",
+              is_correct: false,
+            },
+          ],
+        },
+        {
+          q: 'How many letters are in the word "Banana"?',
+          answers: [
+            {
+              text: "5",
+              is_correct: false,
+            },
+            {
+              text: "7",
+              is_correct: false,
+            },
+            {
+              text: "6",
+              is_correct: true,
+            },
+            {
+              text: "12",
+              is_correct: false,
+            },
+          ],
+        },
+        {
+          q: "Find the missing letter: C_ke",
+          answers: [
+            {
+              text: "e",
+              is_correct: false,
+            },
+            {
+              text: "a",
+              is_correct: true,
+            },
+            {
+              text: "i",
+              is_correct: false,
+            },
+          ],
+        },
+      ],
+      results: [
+        {
+          min: 0,
+          max: 2,
+          title: "Try again!",
+          desc: "Do a little more studying and you may succeed!",
+        },
+        {
+          min: 3,
+          max: 3,
+          title: "Wow, you're a genius!",
+          desc: "Studying has definitely paid off for you!",
+        },
+      ],
+    };
+  },
+  methods: {
+    questionAnswered(isCorrect) {
+      if (isCorrect) {
+        this.totalCorrect++;
+      }
+      this.questionsAnswered++;
+    },
+    resetQuiz() {
+      this.questionsAnswered = 0;
+      this.totalCorrect = 0;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/Questions.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div
+        class="bar"
+        :style="{ width: `${(questionsAnswered / questions.length) * 100}%` }"
+      ></div>
+      <div class="status">
+        {{ questionsAnswered }} out of {{ questions.length }} questions answered
+      </div>
+    </div>
+    <transition-group name="fade">
+      <div
+        class="single-question"
+        v-for="(question, idx) in questions"
+        :key="question.q"
+        v-show="questionsAnswered === idx"
+      >
+        <div class="question">{{ question.q }}</div>
+        <div class="answers">
+          <div
+            class="answer"
+            v-for="answer in question.answers"
+            :key="answer.text"
+            @click="selectAnswer(answer.is_correct)"
+          >
+            {{ answer.text }}
+          </div>
+        </div>
+      </div>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Questions",
+  props: {
+    questions: Array,
+    questionsAnswered: Number,
+  },
+  emits: ["question-answered"],
+  methods: {
+    selectAnswer(isCorrect) {
+      this.$emit("question-answered", isCorrect);
+    },
+  },
+};
+</script>
